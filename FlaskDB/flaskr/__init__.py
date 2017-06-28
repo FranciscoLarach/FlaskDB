c00e8ad18f23d752dbc6e10953c6ea3f1cd405f8
Isidora Vizcaya <isvizcaya@uc.cl>
2017-06-28 14:44:19 -0400

    __init__

--- FlaskDB/flaskr/__init__.py
+++ FlaskDB/flaskr/__init__.py
@@ -49,9 +49,37 @@ def home():
         return render_template('file.html', results=pairs)
 
 
-@app.route("/mongo")
+@app.route("/mongo", methods=['POST'])
 def mongo():
-    query = request.args.get("query")
+    # query = request.args.get("query")
+    num = request.form.get("num")
+    with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
+        json_file = json.load(queries_file)
+    query = json_file[int(num) - 1]["query"]
+
+    if num == "1":
+        print(num)
+        date = request.form.get("date")
+        print(date)
+        print(request.form)
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            return "Error, formato de fecha invalido."
+        query = query.replace("val", date)
+
+    elif num == "2":
+        number = request.form.get("number")
+        k = request.form.get("k")
+        if k.isdecimal() and k != "0" and number.isdecimal():
+            query = query.replace("number", number).replace("k", k)
+        else:
+            return "Ambos k y num deben ser enteros, y k debe ser mayor a 0."
+    
+    elif num == "3":
+        word = request.form.get("word")
+        query = query.replace("palabra", word)
+    print(query)
     results = eval('mongodb.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
     if "find" in query:
