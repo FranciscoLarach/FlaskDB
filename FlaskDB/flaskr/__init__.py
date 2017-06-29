f25b4a77bea373d167434ae8ccc57420d80028c0
Isidora Vizcaya <isvizcaya@uc.cl>
2017-06-28 23:24:21 -0400

    mongo queries

--- FlaskDB/flaskr/__init__.py
+++ FlaskDB/flaskr/__init__.py
@@ -48,44 +48,75 @@ def home():
                   x["query"]) for x in json_file]
         return render_template('file.html', results=pairs)
 
+collection = mongodb["escuchas"]
+collection.create_index([('contenido', 'text')])
 
-@app.route("/mongo", methods=['POST'])
-def mongo():
-    # query = request.args.get("query")
-    num = request.form.get("num")
-    with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
-        json_file = json.load(queries_file)
-    query = json_file[int(num) - 1]["query"]
+@app.route("/query1", methods=['GET'])
+def consulta_1():
+    date = request.args.get('fecha')
+    db = mongodb.escuchas
+    result = json_util.dumps(escuchas.find({"fecha": date}, {"numero":1}))
+    resp = Response(result)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
 
-    if num == "1":
-        print(num)
-        date = request.form.get("date")
-        print(date)
-        print(request.form)
-        try:
-            datetime.datetime.strptime(date, '%Y-%m-%d')
-        except ValueError:
-            return "Error, formato de fecha invalido."
-        query = query.replace("val", date)
+@app.route("/query2", methods=['GET'])
+def consulta_2():
+    num = request.args.get('num')
+    k = request.args.get('k')
+    db = mongodb.escuchas
+    result = json_util.dumps(escuchas.find({"numero": num} ,{"contenido": 1}).limit(k))
+    resp = Response(result)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
 
-    elif num == "2":
-        number = request.form.get("number")
-        k = request.form.get("k")
-        if k.isdecimal() and k != "0" and number.isdecimal():
-            query = query.replace("number", number).replace("k", k)
-        else:
-            return "Ambos k y num deben ser enteros, y k debe ser mayor a 0."
+@app.route("/query3", methods=['GET'])
+def consulta_3():
+    alias = request.args.get('alias')
+    db = mongodb.escuchas
+    result = json_util.dumps(escuchas.find({'$text': {'$search': alias}},{'numero':1, 'fecha':1, 'ciudad':1 , 'contenido':1}))
+    resp = Response(result)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
+
+
+# @app.route("/mongo", methods=['POST'])
+# def mongo():
+#     # query = request.args.get("query")
+#     num = request.form.get("num")
+#     with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
+#         json_file = json.load(queries_file)
+#     query = json_file[int(num) - 1]["query"]
+
+#     if num == "1":
+#         print(num)
+#         date = request.form.get("date")
+#         print(date)
+#         print(request.form)
+#         try:
+#             datetime.datetime.strptime(date, '%Y-%m-%d')
+#         except ValueError:
+#             return "Error, formato de fecha invalido."
+#         query = query.replace("val", date)
+
+#     elif num == "2":
+#         number = request.form.get("number")
+#         k = request.form.get("k")
+#         if k.isdecimal() and k != "0" and number.isdecimal():
+#             query = query.replace("number", number).replace("k", k)
+#         else:
+#             return "Ambos k y num deben ser enteros, y k debe ser mayor a 0."
     
-    elif num == "3":
-        word = request.form.get("word")
-        query = query.replace("palabra", word)
-    print(query)
-    results = eval('mongodb.'+query)
-    results = json_util.dumps(results, sort_keys=True, indent=4)
-    if "find" in query:
-        return render_template('mongo.html', results=results)
-    else:
-        return "ok"
+#     elif num == "3":
+#         word = request.form.get("word")
+#         query = query.replace("palabra", word)
+#     print(query)
+#     results = eval('mongodb.'+query)
+#     results = json_util.dumps(results, sort_keys=True, indent=4)
+#     if "find" in query:
+#         return render_template('mongo.html', results=results)
+#     else:
+#         return "ok"
 
 
 # @app.route("/postgres")
