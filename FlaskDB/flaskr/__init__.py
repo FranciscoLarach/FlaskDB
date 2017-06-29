d6496e2981a4624899bae0edcd69333accee0cbb
Isidora Vizcaya <isvizcaya@uc.cl>
2017-06-28 23:45:11 -0400

    mongo init

--- FlaskDB/flaskr/__init__.py
+++ FlaskDB/flaskr/__init__.py
@@ -23,17 +23,6 @@ MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
 mongodb = client[MONGODATABASE]
 
-''' # Uncomment for postgres connection
-# REPLACE WITH YOUR DATABASE NAME, USER AND PASS
-POSTGRESDATABASE = "dbPostgresEntrega4"
-POSTGRESUSER = "grupo15"
-POSTGRESPASS = "grupo15"
-postgresdb = psycopg2.connect(
-    database=POSTGRESDATABASE,
-    user=POSTGRESUSER,
-    password=POSTGRESPASS)
-'''
-
 #Cambiar por Path Absoluto en el servidor
 QUERIES_FILENAME = '/var/www/flaskr/flaskr/queries'
 
@@ -68,7 +57,7 @@ def consulta_2():
     result = json_util.dumps(escuchas.find({"numero": num} ,{"contenido": 1}).limit(k))
     resp = Response(result)
     resp.headers.add('Access-Control-Allow-Origin', '*')
-    return resp
+    return(resp)
 
 @app.route("/query3", methods=['GET'])
 def consulta_3():
@@ -80,43 +69,43 @@ def consulta_3():
     return resp
 
 
-# @app.route("/mongo", methods=['POST'])
-# def mongo():
-#     # query = request.args.get("query")
-#     num = request.form.get("num")
-#     with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
-#         json_file = json.load(queries_file)
-#     query = json_file[int(num) - 1]["query"]
+@app.route("/mongo", methods=['POST'])
+def mongo():
+    # query = request.args.get("query")
+    num = request.form.get("num")
+    with open(QUERIES_FILENAME, 'r', encoding='utf-8') as queries_file:
+        json_file = json.load(queries_file)
+    query = json_file[int(num) - 1]["query"]
 
-#     if num == "1":
-#         print(num)
-#         date = request.form.get("date")
-#         print(date)
-#         print(request.form)
-#         try:
-#             datetime.datetime.strptime(date, '%Y-%m-%d')
-#         except ValueError:
-#             return "Error, formato de fecha invalido."
-#         query = query.replace("val", date)
+    if num == "1":
+        print(num)
+        date = request.form.get("date")
+        print(date)
+        print(request.form)
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            return "Error, formato de fecha invalido."
+        query = query.replace("val", date)
 
-#     elif num == "2":
-#         number = request.form.get("number")
-#         k = request.form.get("k")
-#         if k.isdecimal() and k != "0" and number.isdecimal():
-#             query = query.replace("number", number).replace("k", k)
-#         else:
-#             return "Ambos k y num deben ser enteros, y k debe ser mayor a 0."
+    elif num == "2":
+        number = request.form.get("number")
+        k = request.form.get("k")
+        if k.isdecimal() and k != "0" and number.isdecimal():
+            query = query.replace("number", number).replace("k", k)
+        else:
+            return "Ambos k y num deben ser enteros, y k debe ser mayor a 0."
     
-#     elif num == "3":
-#         word = request.form.get("word")
-#         query = query.replace("palabra", word)
-#     print(query)
-#     results = eval('mongodb.'+query)
-#     results = json_util.dumps(results, sort_keys=True, indent=4)
-#     if "find" in query:
-#         return render_template('mongo.html', results=results)
-#     else:
-#         return "ok"
+    elif num == "3":
+        word = request.form.get("word")
+        query = query.replace("palabra", word)
+    print(query)
+    results = eval('mongodb.'+query)
+    results = json_util.dumps(results, sort_keys=True, indent=4)
+    if "find" in query:
+        return render_template('mongo.html', results=results)
+    else:
+        return "ok"
 
 
 # @app.route("/postgres")
